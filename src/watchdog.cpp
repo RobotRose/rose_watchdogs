f38a1470d883fc553985237c4afac21b399b4717
ohendriks <hendriks@robot-rose.nl>
2015-03-27 10:34:07 +0100

    Revert back to private nodehandle.

--- src/watchdog.cpp
+++ src/watchdog.cpp
@@ -32,9 +32,12 @@ Watchdog::Watchdog( std::string name, boost::function<void()> callback, bool one
     , n_(ros::NodeHandle())
     , name_(name)
 {
+    // Get a private NodeHandle
+    ros::NodeHandle pn("~");
+
     // Retrieve the timeout parameter
-    std::string timeout_name = ros::names::append(ros::this_node::getName(), ros::names::append(name_, "timeout"));
-    ROS_ASSERT_MSG(n_.getParam(timeout_name, timeout_), "Watchdog timeout parameter '%s' must be specified. Or a timeout value should be provided in the constructor.", timeout_name.c_str());
+    std::string timeout_name = ros::names::append(name_, "timeout");
+    ROS_ASSERT_MSG(pn.getParam(timeout_name, timeout_), "Watchdog timeout parameter '%s' must be specified. Or a timeout value should be provided in the constructor.", timeout_name.c_str());
 
     callback_   = callback;
     oneshot_    = oneshot;
